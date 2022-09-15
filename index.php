3ff6a42f0b65f720e4052fbd2e45389d5b567549
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-15 14:18:42 +0200

    Update index.php
    live omgeving toegevoegd aan index

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <h1><a href="/joshua/index.php">joshua</a></h1>
     <h1><a href="/finn/login.php">finn</a></h1>
     <h1><a href="/testomgeving/index.php">testOmgeving</a></h1>
+    <h1><a href="/liveomgeving/index.php">live Omgeving</a></h1>
 </body>
 </html>
 
