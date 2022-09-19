e3ddc49d5d29faf40053880a582a8725f5fb4b46
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-19 15:53:30 +0200

    Update functions.php
    test update modal koppeling

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -465,7 +465,7 @@ function masseuseInfo()
         <td>
             <div class="row">
                 <div class="col-md-5">
-                    <a data-toggle="modal" data-target="#extraInfo" href="/testomgeving/modals.php">
+                    <a data-toggle="modal" data-target="#extraInfo" href="../modals.php">
                         <i class="ft-eye"></i>
                     </a>
                 </div>
