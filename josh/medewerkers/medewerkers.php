34e4ab6ab008ad78a2f174053461b50d384b5338
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-19 12:35:12 +0200

    Update medewerkers.php
    kleine update paths map Josh

--- josh/medewerkers/medewerkers.php
+++ josh/medewerkers/medewerkers.php
@@ -35,7 +35,7 @@ include "functions.php";
 
 <head>
     <?php
-    include "partials/header.php";
+    include "../partials/header.php";
     ?>
 </head>
 <!-- END : Head-->
@@ -45,7 +45,7 @@ include "functions.php";
 
     <!-- Navbar (Header) Starts-->
     <?php
-    include "partials/navbar.php";
+    include "/partials/navbar.php";
     ?>
     <!-- ////////////////////////////////////////////////////////////////////////////-->
     <!-- / main menu-->
@@ -129,7 +129,7 @@ include "functions.php";
     <div class="sidenav-overlay"></div>
     <div class="drag-target"></div>
     <?php
-    include "partials/footer.php";
+    include "/partials/footer.php";
     ?>
     <script>
         /*
