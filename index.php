29ad04b0a0fd071a29f9b66e46214f2dcdb516bb
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-19 12:02:03 +0200

    Update index.php
    index updated goede path naar map Josh

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1><a href="/steef/login.php">steef</a></h1>
-    <h1><a href="/joshua/index.php">joshua</a></h1>
+    <h1><a href="/josh/index.php">joshua</a></h1>
     <h1><a href="/finn/login.php">finn</a></h1>
     <h1><a href="/testomgeving/index.php">testOmgeving</a></h1>
     <h1><a href="/liveomgeving/index.php">live Omgeving</a></h1>
