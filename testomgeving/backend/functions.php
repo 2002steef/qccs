1b044a17d615cd2f6dc5368e4430bc94233b38fb
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-20 15:49:29 +0200

    Update functions.php
    kleine update functions

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -468,29 +468,10 @@ function masseuseInfo()
                 </a>
             </td>
             </tbody>
+         
         <?php
+        
     }
-        ?>
-        <div class="modal fade text-left" id="info<?= $masseuse["masseuseID"] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="myModalLabel2"><i class="ft-bookmark mr-2"></i>Basic Modal</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true"><i class="ft-x font-medium-2 text-bold-700"></i></span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn bg-light-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
     }
 
     function GetCompanyName()
