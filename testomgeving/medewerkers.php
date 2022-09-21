d8e0c8832063799cbe64573fb5a0f0ad5a59f294
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-21 11:06:38 +0200

    Update medewerkers.php
    revert update

--- testomgeving/medewerkers.php
+++ testomgeving/medewerkers.php
@@ -111,25 +111,7 @@ if (!isset($_SESSION["loggedin"])) {
             </div>
         </div>
         <!-- END : End Main Content-->
-        <div class="modal fade text-left" id="info<?= $_GET["masseuseID"]?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel2"><i class="ft-bookmark mr-2"></i>Basic Modal</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true"><i class="ft-x font-medium-2 text-bold-700"></i></span>
-                </button>
-            </div>
-            <div class="modal-body">
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn bg-light-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-    </div>
-</div> 
+
         <!-- Scroll to top button -->
         <button class="btn btn-primary scroll-top" type="button"><i class="ft-arrow-up"></i></button>
 
