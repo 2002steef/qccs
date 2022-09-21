ae90d789f4faee27b39dd2578cfae267efc46613
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-21 13:05:52 +0200

    Update functions.php
    testen modal

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -455,7 +455,7 @@ function masseuseInfo()
         <tr>
             <td><?= $masseuse["masseuseID"] ?></td>
             <td><?= $masseuse["voornaam"] ?></td>
-            <td><a href="modals.php?1" data-toggle="modal" data-target="#info1">
+            <td><a href="modals.php" data-toggle="modal" data-target="#info1">
                             <i class="ft-eye" data-toggle="tooltip" data-original-title="Info bekijken" data-placement="bottom"></i>
                         </a></td>
                         <td></td>
