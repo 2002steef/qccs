6ba54d3bbf9e56fab7e22c220625076a08f457ef
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-26 14:22:14 +0200

    Update navbar.php
    update sidebar

--- testomgeving/partials/navbar.php
+++ testomgeving/partials/navbar.php
@@ -9,11 +9,11 @@ $row = Getuser();
 ?>
 <!-- END : Head-->
 <!-- BEGIN : Body-->
-<body class="vertical-layout vertical-menu 2-columns  navbar-sticky "
+<body class="vertical-layout vertical-menu 2-columns bg-transparant navbar-sticky "
      data-menu="vertical-menu" data-col="2-columns">
 
 <!-- Navbar (Header) Starts-->
-<nav class="navbar navbar-expand-lg  header-navbar navbar-static">
+<nav class="navbar navbar-expand-lg  header-navbar bg-transparant navbar-static">
     <div class="container-fluid navbar-wrapper">
         <div class="navbar-header d-flex">
             <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center"
