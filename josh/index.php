fdc520bff9109053cc41132d721b00b96b73e858
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-26 11:58:34 +0200

    Update index.php
    update layout

--- josh/index.php
+++ josh/index.php
@@ -69,7 +69,7 @@ include "backend/voucherFunctions.php" ;
                     <!-- Block level buttons start -->
                     <section id="block-level-buttons">
                         <div class="row">
-                            <div class="col-12 ">
+                            <div class="col-6 ">
                                 <div class="card">
                                     <div class="card-header">
                                         <h4 class="card-title">Voucher sturen</h4>
