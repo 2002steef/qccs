04fd09fecef29a1491c7d9c8312c061c3cbbc812
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-26 13:32:15 +0200

    Update functions.php
    button een link gemaakt met button styling

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -468,9 +468,9 @@ function masseuseInfo()
         <tr>
             <td><?= $masseuse["masseuseID"] ?></td>
             <td colspan="5"><?= $masseuse["voornaam"] ?></td>
-            <td><button class="col-md-6 btn btn-outline-primary" href="masseuse_profiel.php?<?= $masseuse["masseuseID"] ?>">
+            <td><a class="col-md-6 btn btn-outline-primary" href="masseuse_profiel.php?<?= $masseuse["masseuseID"] ?>">
                             Meer info
-                        </a></button>
+                        </a></a>
         </tr>
     <?php }
 }
