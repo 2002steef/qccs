c34336a1866d280bbc6090a7f3570d003459bc2d
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-27 14:09:39 +0200

    Update navbar.php
    text update foto

--- testomgeving/partials/navbar.php
+++ testomgeving/partials/navbar.php
@@ -93,7 +93,7 @@ $row = Getuser();
         <div class="logo clearfix">
                 <a class="logo-text float-left" href="bedrijfs_overzicht.php">
                     <div class="logo-img"><img height="50px" width="50px" src="assets/img/BMA-Logo.png"/></div>
-                    <span class="text"  >BMA</span>
+                    <span class="light-gray"  >BMA</span>
                 </a>
             <a class="nav-toggle d-none d-lg-none d-xl-block" id="sidebarToggle" href="javascript:;"><i
                         class="toggle-icon ft-toggle-right" data-toggle="expanded"></i></a>
@@ -105,7 +105,7 @@ $row = Getuser();
     <!--     Sidebar Header Ends-->
     <!--     / main menu header-->
     <!--    main menu content-->
-    <div class="sidebar-content main-menu-content  " >
+    <div class="sidebar-content main-menu-content" >
         <div class="nav-container">
             <ul class="navigation navigation-main " id="main-menu-navigation" data-menu="menu-navigation">
                 <li class="nav-item ">
