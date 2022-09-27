c57ad12a99c7de9d53e8ffe16df401803a4314d4
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-27 14:43:30 +0200

    Update navbar.php
    text update test

--- testomgeving/partials/navbar.php
+++ testomgeving/partials/navbar.php
@@ -9,17 +9,17 @@ $row = Getuser();
 ?>
 <!-- END : Head-->
 <!-- BEGIN : Body-->
-<body class="vertical-layout vertical-menu 2-columns navbar-sticky "
+<body class="vertical-layout vertical-menu 2-columns navbar-sticky light-gray"
      data-menu="vertical-menu" data-col="2-columns">
 
 <!-- Navbar (Header) Starts-->
 <nav class="navbar navbar-expand-lg  header-navbar  navbar-static light-gray" >
     <div class="container-fluid navbar-wrapper">
         <div class="navbar-header d-flex">
-            <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center light-grey"
-                 data-toggle="collapse"><i class="ft-menu font-medium-3 light-grey"></i></div>
+            <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center light-gray"
+                 data-toggle="collapse"><i class="ft-menu font-medium-3 light-gray"></i></div>
             <ul class="navbar-nav">
-                <li class="nav-item mr-2 d-none d-lg-block"><a class="nav-link apptogglefullscreen light-grey"
+                <li class="nav-item mr-2 d-none d-lg-block"><a class="nav-link apptogglefullscreen light-gray"
                                                                id="navbar-fullscreen" href="javascript:;"><i
                                 class="ft-maximize font-medium-3 light-gray"></i></a></li>
                 <li class="nav-item nav-search"><a class="nav-link nav-link-search" href="javascript:"><i
