aa0ee717c8ef642b376212083117546a8f675f70
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-28 16:50:53 +0200

    Update navbar.php
    update profile picture

--- testomgeving/partials/navbar.php
+++ testomgeving/partials/navbar.php
@@ -40,7 +40,7 @@ $masseuse = GetMasseuse();
                                 <div class="user d-md-flex d-none mr-2"><span class="text-right light-gray"><?php if (isset($_SESSION["voornaam"])) {
                                                                                                                 echo $_SESSION["voornaam"];
                                                                                                             } ?></span><span class="text-right text-muted font-small-3 light-gray">Beschikbaar</span></div>
-                                <img class="avatar" src="../img/profile/profile-portrait-2.jpg" alt="avatar" height="35" width="35">
+                                <img class="avatar" src="assets/img/BMA-Logo.png" alt="avatar" height="35" width="35">
                             </a>
                             <div class="dropdown-menu text-left dropdown-menu-right m-0 pb-0" aria-labelledby="dropdownBasic2"><a class="dropdown-item" href="page-account-settings.php">
                                     <div class="d-flex align-items-center"><i class="ft-edit mr-2 light-gray"></i><span>Profiel settings</span></div>
