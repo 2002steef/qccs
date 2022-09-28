a032f6e83d0dbeb86da9339f23640f138f20fdfe
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-28 17:02:35 +0200

    Update verwerk_login.php
    kleine test login pagina

--- testomgeving/backend/verwerk_login.php
+++ testomgeving/backend/verwerk_login.php
@@ -8,24 +8,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location:../index.php?login=leeg");
     }
     if (isset($_POST['email'], $_POST['wachtwoord'])) {
-       $_SESSION['email'] = $_POST['email'];
+        $_SESSION['email'] = $_POST['email'];
         if ($stmt = $mysqli->prepare('SELECT userID, `userName`, `Password`, `email` FROM `medewerkers` WHERE email = ?')) {
             // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
             $stmt->bind_param('s', $_POST['email']);
             $stmt->execute();
-        
+
             // Store the result so we can check if the account exists in the database.
             $stmt->store_result();
 
             if ($stmt->num_rows > 0) {
-                $stmt->bind_result($id,$userName, $password, $email);
-//            $stmt->bind_result($id, $password);
+                $stmt->bind_result($id, $userName, $password, $email);
+                //            $stmt->bind_result($id, $password);
                 $stmt->fetch();
                 // Account exists, now we verify the password.
                 // Note: remember to use password_hash in your registration file to store the hashed passwords.
-//            if (password_verify($_POST['wachtwoord'], $password)) {
+                //            if (password_verify($_POST['wachtwoord'], $password)) {
                 if ($_POST['wachtwoord'] == $password) {
-//                $row = $stmt->get_result();
+                    //                $row = $stmt->get_result();
                     // Verification success! User has logged-in!
                     // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
                     session_regenerate_id();
@@ -33,12 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $_SESSION['name'] = $_POST['email'];
                     $_SESSION['id'] = $id;
                     $_SESSION['status'] = "medewerker";
-//            echo 'Welcome ' . $_SESSION['name'] . '!';
+                    //            echo 'Welcome ' . $_SESSION['name'] . '!';
                     if (!empty($_POST["remember_me"])) {
                         setcookie("username", $_POST["email"], time() + 3600);
                         setcookie("password", $_POST["wachtwoord"], time() + 3600);
-//                    echo $_COOKIE['username'];
-//                    echo $_COOKIE['password'];
+                        //                    echo $_COOKIE['username'];
+                        //                    echo $_COOKIE['password'];
                         echo "Cookies Set Successfuly";
                     } else {
                         setcookie("username", "");
@@ -51,33 +51,72 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     //     //header("Location: ../device_confirmations.php");
 
                     // } else {
-                        if ($_SESSION['memb_of'] == 0) {
-                            header("Location:../medewerkers.php?");
-                        } else {
-                            if (isset($_SESSION['auth']) && isset($_SESSION['memb_of'])) {
-                                if ($_SESSION['auth'] == "Werknemer") {
-                                    header("Location:../klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-                                } else if ($_SESSION['auth'] == "Bedrijfsleider") {
-                                    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-                                }  else if ($_SESSION['auth'] == "user") {
-                                    header("Location:../klant_overzicht.php?custof=$memb_of&membof=$memb_of");
-                                }
+                    if ($_SESSION['memb_of'] == 0) {
+                        header("Location:../medewerkers.php?");
+                    } else {
+                        if (isset($_SESSION['auth']) && isset($_SESSION['memb_of'])) {
+                            if ($_SESSION['auth'] == "Werknemer") {
+                                header("Location:../klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+                            } else if ($_SESSION['auth'] == "Bedrijfsleider") {
+                                header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+                            } else if ($_SESSION['auth'] == "user") {
+                                header("Location:../klant_overzicht.php?custof=$memb_of&membof=$memb_of");
                             }
                         }
+                    }
 
                     // }
                 } else {
                     // Incorrect password
-//                $message = 'Je hebt geen geldige combinatie van email en wachtwoord';
+                    //                $message = 'Je hebt geen geldige combinatie van email en wachtwoord';
                     header("Location:../index.php?login=foutecombi");
                 }
-            } else {
-                // Incorrect username
-//            $message = 'Je hebt geen geldige combinatie van email en wachtwoord';
-                header("Location:../index.php?login=foutecombi");
+            } elseif ($stmt->num_rows === 0) {
+                $stmt = $mysqli->prepare('SELECT `masseuseID`, `userName`, `Password`, `email`, `voornaam` FROM `masseuses` WHERE ?');
+                $stmt->bind_param('s', $_POST['email']);
+                $stmt->execute();
+                $stmt->store_result();
+                if ($stmt->num_rows > 0) {
+                    $stmt->bind_result($id, $userName, $password, $email);
+                    //            $stmt->bind_result($id, $password);
+                    $stmt->fetch();
+                    // Account exists, now we verify the password.
+                    // Note: remember to use password_hash in your registration file to store the hashed passwords.
+                    //            if (password_verify($_POST['wachtwoord'], $password)) {
+                    if ($_POST['wachtwoord'] == $password) {
+                        //                $row = $stmt->get_result();
+                        // Verification success! User has logged-in!
+                        // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                        session_regenerate_id();
+                        $_SESSION['loggedin'] = true;
+                        $_SESSION['name'] = $_POST['email'];
+                        $_SESSION['id'] = $id;
+                        $_SESSION['status'] = "bedrijf";
+                        //            echo 'Welcome ' . $_SESSION['name'] . '!';
+                        if (!empty($_POST["remember_me"])) {
+                            setcookie("username", $_POST["email"], time() + 3600);
+                            setcookie("password", $_POST["wachtwoord"], time() + 3600);
+                            //                    echo $_COOKIE['username'];
+                            //                    echo $_COOKIE['password'];
+                            echo "Cookies Set Successfuly";
+                        } else {
+                            setcookie("username", "");
+                            setcookie("password", "");
+                            echo "Cookies Not Set";
+                        }
+                        // echo "Je bent ingelogd";
+                        // if (!empty($googlecode)) {
+                        //     header("Location: ../googlecode.php");
+                        //     //header("Location: ../device_confirmations.php");
+
+                        // } else {
+                        if ($_SESSION['status'] == "bedrijf") {
+                            header("Location:../bma_bedrijfs_klanten_overzicht.php?");
+                        } 
+                    }
+                }
             }
             $stmt->close();
         }
     }
 }
-
