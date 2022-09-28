98d2e720cbef4dff936d6e54b2f6ca1385163ca4
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-28 14:47:22 +0200

    Update verwerk_login.php
    bedrijven login test

--- testomgeving/backend/verwerk_login.php
+++ testomgeving/backend/verwerk_login.php
@@ -51,47 +51,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     // Incorrect password
                     header("Location:../index.php?login=foutecombi");
                 }
-            }elseif($stmt->num_rows == 0 )
-            {
-                $stmt = $mysqli->prepare(('SELECT `bedrijfID`, `userName`, `Password`, `email` FROM `bedrijven` WHERE ?'));
-                 // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-                 $stmt->bind_param('s', $_POST['email']);
-                 $stmt->execute();
-             
-                // Store the result so we can check if the account exists in the database.
-                if ($stmt->num_rows > 0) {
-                    $stmt->bind_result($id,$userName, $password, $email);
-                    $stmt->fetch();
-                    // Account exists, now we verify the password.
-                    // Note: remember to use password_hash in your registration file to store the hashed passwords.
-                    if ($_POST['wachtwoord'] == $password) {
-    
-                        // Verification success! User has logged-in!
-                        // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-                        session_regenerate_id();
-                        $_SESSION['loggedin'] = true;
-                        $_SESSION['name'] = $_POST['email'];
-                        $_SESSION['id'] = $id;
-                        $_SESSION['status'] = "bedrijf";
-                        if (!empty($_POST["remember_me"])) {
-                            setcookie("username", $_POST["email"], time() + 3600);
-                            setcookie("password", $_POST["wachtwoord"], time() + 3600);
-                            echo "Cookies Set Successfuly";
-                        } else {
-                            setcookie("username", "");
-                            setcookie("password", "");
-                            echo "Cookies Not Set";
-                        }
-                    } 
-                    if (isset($_SESSION['status']) ) {
-                        if ($_SESSION['status'] == "bedrijf") {
-                            header("Location:../bma_bedrijfs_klanten_overzicht.php");
-                        } 
-                    }else {
-                        // Incorrect password
-                        header("Location:../index.php?login=foutecombi");
-                    }
-                }
             }
         }
     }
