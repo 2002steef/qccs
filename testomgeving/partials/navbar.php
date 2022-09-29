549385ce870914ef81d1dc56af023bd8f0217f28
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-29 11:41:07 +0200

    Update navbar.php
    navbar update test profielfoto change

--- testomgeving/partials/navbar.php
+++ testomgeving/partials/navbar.php
@@ -40,7 +40,7 @@ $masseuse = GetMasseuse();
                                 <div class="user d-md-flex d-none mr-2"><span class="text-right light-gray"><?php if (isset($_SESSION["voornaam"])) {
                                                                                                                 echo $_SESSION["voornaam"];
                                                                                                             } ?></span><span class="text-right text-muted font-small-3 light-gray">Beschikbaar</span></div>
-                                <img class="avatar" src="assets/img/BMA-Logo.png" alt="avatar" height="35" width="35">
+                                <img class="avatar" src="img/uploads/<?=$masseuse["profielFoto"] ?>" alt="avatar" height="35" width="35">
                             </a>
                             <div class="dropdown-menu text-left dropdown-menu-right m-0 pb-0" aria-labelledby="dropdownBasic2"><a class="dropdown-item" href="page-account-settings.php">
                                     <div class="d-flex align-items-center"><i class="ft-edit mr-2 light-gray"></i><span>Profiel settings</span></div>
@@ -92,7 +92,7 @@ $masseuse = GetMasseuse();
                             } elseif ($_SESSION["status"] == "masseuse") {
                 ?>
                     <li>
-                        <a class="light-gray" href="masseuse_profiel.php">
+                        <a class="light-gray" href="masseuse_profiel.php?masseuseID=<?=$_SESSION["id"] ?>">
                             <i class="ft-user light-gray"></i>
                             <span class=" menu-title text light-gray">Masseuse Profiel</span>
                         </a>
