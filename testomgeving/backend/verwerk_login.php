8ef44c0058cf4fc7ecf137a0e44c893391aa0248
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-09-29 09:34:47 +0200

    Update verwerk_login.php
    function update login pagina

--- testomgeving/backend/verwerk_login.php
+++ testomgeving/backend/verwerk_login.php
@@ -160,7 +160,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     }
                 }
             }
-            $stmt->close();
+            $stmtMw && $stmtBd && $stmtMs->close();
         }
     }
 }
