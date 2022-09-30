b94d8be994ff3770907fd87493deff997c8f04ef
2002steef <63845761+2002steef@users.noreply.github.com>
2022-09-30 10:56:59 +0200

    Update voucherFunctions.php

--- josh/backend/voucherFunctions.php
+++ josh/backend/voucherFunctions.php
@@ -1,8 +1,7 @@
 <?php
-include "backend/db.php";
-echo('<script>console.log("test")</script>');
+
 function createRandomVoucher(
-    int $length = 10,
+    int $length = 18,
     string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 ): string {
     if ($length < 1) {
@@ -11,35 +10,18 @@ function createRandomVoucher(
     $pieces = [];
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
-        $pieces[] = $keyspace[random_int(0, $max)];
+        $pieces []= $keyspace[random_int(0, $max)];
     }
     return implode('', $pieces);
-    if (isset($_POST['VoucherSturen'])) {
-        $token = createRandomVoucher();
-        $email = $_POST["KlantMail"];
-        if ($email) {
-            $to = $email;
-            $subject = "Voucher code";
-            $msg = "Uw voucher code is . $token ";
-            $msg = wordwrap($msg, 70);
-            $headers = "From: Josh@qccs.nl";
-            mail($to, $subject, $msg, $headers);
-
-
-            $stmt = $mysqli->prepare(
-                "INSERT INTO `vouchers` (`userID`, `masseuseID`, `voucherCode`, `status`) VALUES (?, ?, ?, ?)"
-            );
-            $voucher = createRandomVoucher();
-            $userID = 1;
-            $masseuseID = 2;
-            $stmt->bind_param("iisi", $userID, $masseuseID, $voucher, 1);
-            $stmt->execute();
-            if ($stmt->num_rows > 0) {
-                echo "Code gemaakt en in database gezet";
-            }
-
-
-            // header('location:index.php');
-        }
-    }
 }
+
+
+
+
+// $stmt = $mysqli->prepare("INSERT INTO `vouchers`(`userID`, `voucher`) VALUES ('1',?)");
+// $voucher = createRandomVoucher();
+// $stmt->bind_param("s", $voucher);
+// $stmt->execute();
+// if ($stmt->num_rows > 0) {
+//     echo "Code gemaakt en in database gezet";
+// }
