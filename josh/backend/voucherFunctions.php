89f93e8b8534c3d6844e33b9273a2cde4d258bf1
2002steef <steef.vanderpoel@ictmbo.nl>
2022-09-30 11:44:06 +0200

    nog meer updates

--- josh/backend/voucherFunctions.php
+++ josh/backend/voucherFunctions.php
@@ -14,26 +14,13 @@ function createRandomVoucher(
     }
     return implode('', $pieces);
 
-    if (isset($_POST['VoucherSturen'])) {
-        $token = createRandomVoucher();
-        $email = $_POST["KlantMail"];
-        if ($email) {
-            $to = $email;
-            $subject = "Voucher code";
-            $msg = "Uw voucher code is . $token ";
-            $msg = wordwrap($msg, 70);
-            $headers = "From: Josh@qccs.nl";
-            mail($to, $subject, $msg, $headers);
-            header('location:../index.php');
 
-            // $stmt = $mysqli->prepare("INSERT INTO `vouchers`(`userID`, `voucher`) VALUES ('1',?)");
-            // $voucher = createRandomVoucher();
-            // $stmt->bind_param("s", $voucher);
-            // $stmt->execute();
-            // if ($stmt->num_rows > 0) {
-            //     echo "Code gemaakt en in database gezet";
-            // }
+    // $stmt = $mysqli->prepare("INSERT INTO `vouchers`(`userID`, `voucher`) VALUES ('1',?)");
+    // $voucher = createRandomVoucher();
+    // $stmt->bind_param("s", $voucher);
+    // $stmt->execute();
+    // if ($stmt->num_rows > 0) {
+    //     echo "Code gemaakt en in database gezet";
+    // }
 
-        }
-    }
 }
