e0abba3d1998bd1a97ff946f5e2cad4742b92847
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-03 10:04:59 +0200

    medewerker, functions
    tabel test grootte update

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -540,7 +540,7 @@ function masseuseInfo()
     while ($masseuse = $resultMasseuse->fetch_array()) { ?>
         <tr>
             <td ><img src="img/uploads/<?= $masseuse["profielFoto"] ?>" width="150px" height="150px" alt="masseuse foto"></td>
-            <td ><?= $masseuse["paragraafje"] ?></td>
+            <td class="col-3" ><?= $masseuse["paragraafje"] ?></td>
             <td ><a class="btn btn-outline-light-grey" href="masseuse_profiel.php?masseuseID=<?= $masseuse["masseuseID"] ?>">
                     Meer info
                 </a>
