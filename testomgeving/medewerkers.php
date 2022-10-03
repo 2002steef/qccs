65c9fb37670c38423f3fc01d7ac356ca298942be
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-03 10:00:53 +0200

    Update medewerkers.php
    tabel test update

--- testomgeving/medewerkers.php
+++ testomgeving/medewerkers.php
@@ -82,7 +82,7 @@ if (!isset($_SESSION["loggedin"])) {
                                         <div class="tab-pane fade mt-2 show active light-gray" id="Particulier" role="tabpanel" aria-labelledby="account-tab">
                                             <div class="row">
                                                 <div class="col-12">
-                                                    <div class="table">
+                                                    <div class="table-responsive">
                                                         <table class="table thead-dark table-bordered file-export">
                                                             <thead>
                                                                 <tr>
