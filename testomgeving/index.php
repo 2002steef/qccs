3f88a0fb0bfe00ac4d9eb54589353e1be7e766b0
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-05 09:16:09 +0200

    Update index.php
    index kleur veranderd

--- testomgeving/index.php
+++ testomgeving/index.php
@@ -12,7 +12,7 @@ include "partials/header.php";
 <!-- END : Head-->
 <!-- BEGIN : Body-->
 
-<body class="horizontal-layout horizontal-menu horizontal-menu-padding navbar-static 1-column auth-page navbar-static layout-dark layout-transparent bg-glass-2 blank-page" data-bg-img="bg-glass-2" data-open="hover" data-menu="horizontal-menu" data-col="1-column">
+<body class="horizontal-layout horizontal-menu horizontal-menu-padding navbar-static 1-column auth-page navbar-static" data-bg-img="bg-glass-2" data-open="hover" data-menu="horizontal-menu" data-col="1-column">
     <!-- ////////////////////////////////////////////////////////////////////////////-->
     <div class="wrapper">
         <div class="main-panel">
