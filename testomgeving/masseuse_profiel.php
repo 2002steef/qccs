72a53e02b4efe838776a88556849734aeceecc21
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-05 10:37:39 +0200

    Update masseuse_profiel.php
    maps update

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -167,7 +167,6 @@ include "partials/header.php";
                                                     <div class="mapouter">
                                                         <div class="gmap_canvas">
                                                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d79350.60592332779!2d4.9686496953033705!3d51.57361500652063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6be1d126870f3%3A0xaa2d2182f3b09727!2sTilburg!5e0!3m2!1snl!2snl!4v1664958888148!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                                                <a href="https://2piratebay.org">BMA</a><br><style>.mapouter{position:relative;text-align:right;height: 400px;width: 800px;}</style><a href="https://www.embedgooglemap.net"></a>
                                                                 <style>.gmap_canvas {overflow:auto;background:none!important;height: 475px;width: 800px;}</style>
                                                             </div>
                                                         </div>
