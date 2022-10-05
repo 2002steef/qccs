01b20b7cfd76c218a6c57c31259ebdf158de6edd
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-05 10:28:42 +0200

    Update masseuse_profiel.php
    hoogte/ breedte maps

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -167,8 +167,8 @@ include "partials/header.php";
                                                     <div class="mapouter">
                                                         <div class="gmap_canvas">
                                                             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                                                                <a href="https://2piratebay.org">pirate bay</a><br><style>.mapouter{position:relative;text-align:right;height: 500px;width: 600px;}</style><a href="https://www.embedgooglemap.net"></a>
-                                                                <style>.gmap_canvas {overflow:hidden;background:none!important;height: 500px;width: 600px;}</style>
+                                                                <a href="https://2piratebay.org">pirate bay</a><br><style>.mapouter{position:relative;text-align:right;height: 300px;width: 800px;}</style><a href="https://www.embedgooglemap.net"></a>
+                                                                <style>.gmap_canvas {overflow:hidden;background:none!important;height: 300px;width: 800px;}</style>
                                                             </div>
                                                         </div>
                                                     </div>
