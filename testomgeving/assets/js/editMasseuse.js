cd412ffb2d2210d092d3cc02889c3b868ca1c9c8
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-05 14:31:11 +0200

    editMasseuse.js, script.js
    nieuwe script pagina aangemaakt

---
+++ testomgeving/assets/js/editMasseuse.js
@@ -0,0 +1,6 @@
+function ClickEdit() {
+    document.getElementById("editMasseuse").readOnly = false;
+    document.getElementById("editMasseuseTel").readOnly = false;
+    document.getElementById("editMasseuseMail").readOnly = false;
+    document.getElementById("editMasseuseParagraafje").readOnly = false;
+};
