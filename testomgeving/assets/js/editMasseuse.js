65f2896c172e6b3c285d1fb5d4e87be2e6f2474a
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 10:57:08 +0200

    Update editMasseuse.js

--- testomgeving/assets/js/editMasseuse.js
+++ testomgeving/assets/js/editMasseuse.js
@@ -1,5 +1,10 @@
 function ClickEdit() {
-   
+    var x = document.getElementById("btnSaveMasseuse");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
     document.getElementById("editMasseuse").readOnly = false;
     document.getElementById("editMasseuseTel").readOnly = false;
     document.getElementById("editMasseuseMail").readOnly = false;
