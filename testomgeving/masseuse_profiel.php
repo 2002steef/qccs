9c6a8c75119c0c733e3950b5973f93ddff8ce87e
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 11:46:43 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -123,9 +123,9 @@ include "partials/header.php";
                                                     <h4 class="card-title m-0">Adress</h4>
                                                     <span class="cursor-pointer"><i class="ft-more-vertical-"></i></span>
                                                 </div>
-                                                <div class="card-content scrolltext">
+                                                <div class="card-content hidescroll ">
                                                     <div class="card-body">
-                                                        <p class="m-0 hidescroll">
+                                                        <p class="m-0 scrolltext">
                                                             <input type="hidden" value="<?= $masseuse["masseuseID"] ?>" name="masseuseID">
                                                             <label for="postcode">Postcode:</label>
                                                             <input name="postcode" value="<?= $masseuse["postcode"]; ?>" class="form-control-plaintext"> </input>
