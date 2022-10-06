fce20dd7dbee483569022ebdbcc84121b7c7f789
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 12:09:52 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -133,9 +133,9 @@ include "partials/header.php";
                                                                 <label for="plaats">Plaats : </label>
                                                                 <input class="form-control-plaintext " type="text" readonly value="<?= $masseuse["plaats"]; ?>" name="plaats"></input>
                                                                 <label for="straat">Straat & huisnummer</label>
-                                                                <input type="text" name="straat" readonly value="<?= $masseuse["straat"]; ?>" class="form-control-plaintext col-6 "></input>
-                                                                <input class="form-control-plaintext col-2" readonly type="text" name="huisNummer" value="<?= $masseuse["huisNummer"]; ?>"></input>
-                                                                <input class="form-control-plaintext col-2" readonly type="text" name="huisNummerToevoeging" value="<?= $masseuse["huisNummerToevoeging"]; ?>"></input>
+                                                                <input type="text" name="straat" readonly value="<?= $masseuse["straat"]; ?>" class="form-control-plaintext "></input>
+                                                                <input class="form-control-plaintext " readonly type="text" name="huisNummer" value="<?= $masseuse["huisNummer"]; ?>"></input>
+                                                                <input class="form-control-plaintext " readonly type="text" name="huisNummerToevoeging" value="<?= $masseuse["huisNummerToevoeging"]; ?>"></input>
                                                             </div>
                                                         </div>
                                                     </div>
