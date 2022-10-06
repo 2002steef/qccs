8f7081690d3371d55c7d9441dfdd08e82d59c67d
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 11:00:35 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -132,7 +132,7 @@ include "partials/header.php";
                                                             <label for="plaats">Plaats : </label>
                                                             <input type="text" value="<?= $masseuse["plaats"]; ?>" name="plaats"></input>
                                                             <label for="straat">Straat & huisnummer</label>
-                                                            <input type="text" name="straat" value="straat" class="form-control-plaintext"></input><input class="form-control-plaintext" type="text" name="huisNummer" value="huisNummer"></input><input class="form-control-plaintext" type="text" name="huisNummerToevoeging" value="huisNummerToevoeging"></input>
+                                                            <input type="text" name="straat" value="straat" class="form-control-plaintext col-2"></input><input class="form-control-plaintext" type="text" name="huisNummer" value="huisNummer" col-2></input><input class="form-control-plaintext col-2" type="text" name="huisNummerToevoeging" value="huisNummerToevoeging"></input>
                                                         </p>
                                                     </div>
                                                 </div>
