1fd88260ae5dad364b01f7b9044dc20983f3f2dc
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 11:53:44 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -124,15 +124,15 @@ include "partials/header.php";
                                                     <span class="cursor-pointer"><i class="ft-more-vertical-"></i></span>
                                                 </div>
                                                 <div class="card-content hidescroll ">
-                                                    <div class="card-body">
-                                                        <div class="m-0 scrolltext">
+                                                    <div class="card-body ">
+                                                        <div class="m-0 ">
                                                             <input type="hidden" value="<?= $masseuse["masseuseID"] ?>" name="masseuseID">
                                                             <label for="postcode">Postcode:</label>
                                                             <input name="postcode" value="<?= $masseuse["postcode"]; ?>" class="form-control-plaintext"> </input>
                                                             <label for="plaats">Plaats : </label>
                                                             <input class="form-control-plaintext " type="text" readonly value="<?= $masseuse["plaats"]; ?>" name="plaats"></input>
                                                             <label for="straat">Straat & huisnummer</label>
-                                                            <div class=" form-row">
+                                                            <div class=" row scrolltext">
                                                                 <input type="text" name="straat" readonly value="<?= $masseuse["straat"]; ?>" class="form-control-plaintext col"></input>
                                                                 <input class="form-control-plaintext col" readonly type="text" name="huisNummer" value="<?= $masseuse["huisNummer"]; ?>"></input>
                                                                 <input class="form-control-plaintext col" readonly type="text" name="huisNummerToevoeging" value="<?= $masseuse["huisNummerToevoeging"]; ?>"></input>
