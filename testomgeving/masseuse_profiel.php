925a49a4378482d169082d250dd52e51d4edb1bb
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-06 12:10:18 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -126,13 +126,14 @@ include "partials/header.php";
                                                 <div class="card-content  hidescroll">
                                                     <div class="card-body scrolltext ">
                                                         <div class="m-0 ">
-                                                            <div class="row">
+                                                            
                                                                 <input type="hidden" value="<?= $masseuse["masseuseID"] ?>" name="masseuseID">
                                                                 <label for="postcode">Postcode:</label>
                                                                 <input name="postcode" readonly value="<?= $masseuse["postcode"]; ?>" class="form-control-plaintext"> </input>
                                                                 <label for="plaats">Plaats : </label>
                                                                 <input class="form-control-plaintext " type="text" readonly value="<?= $masseuse["plaats"]; ?>" name="plaats"></input>
                                                                 <label for="straat">Straat & huisnummer</label>
+                                                                <div class="row">
                                                                 <input type="text" name="straat" readonly value="<?= $masseuse["straat"]; ?>" class="form-control-plaintext "></input>
                                                                 <input class="form-control-plaintext " readonly type="text" name="huisNummer" value="<?= $masseuse["huisNummer"]; ?>"></input>
                                                                 <input class="form-control-plaintext " readonly type="text" name="huisNummerToevoeging" value="<?= $masseuse["huisNummerToevoeging"]; ?>"></input>
