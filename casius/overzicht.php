845cd5510256de257d67f1f7077a8a736bcbb42e
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-10 12:18:13 +0200

    Update overzicht.php

--- casius/overzicht.php
+++ casius/overzicht.php
@@ -232,8 +232,8 @@ $klant = klantInfo();
                                                 <div class="row">
                                                     <div class="col-md-6 col-12">
                                                         <div class="form-group row">
-                                                            <label class="col-md-6 col-form-label" for="horizontal-form-1">Voornaam</label>
-                                                            <div class="col-md-6">
+                                                            <label class="col-md-4 col-form-label" for="horizontal-form-1">Voornaam</label>
+                                                            <div class="col-md-8">
                                                                 <input type="text" class="form-control square" id="horizontal-form-1" name="first-name">
                                                             </div>
                                                         </div>
