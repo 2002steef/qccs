f4642bea46682b75406010b90669d06f382c93ec
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-10 09:50:20 +0200

    Create db.php

---
+++ casius/backend/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "admin_casius";
+$password = "9Y%k98sf3";
+$db = "casius";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
