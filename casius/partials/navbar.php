c8b278f137ecabf48b78ef51e8e94cc9219ea289
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-10 11:16:57 +0200

    Update navbar.php

--- casius/partials/navbar.php
+++ casius/partials/navbar.php
@@ -17,12 +17,12 @@ include "header.php";
             <div class="navbar-header d-flex">
                 <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center " data-toggle="collapse"><i class="ft-menu font-medium-3 "></i></div>
                 <ul class="navbar-nav">
-                    <li class="nav-item mr-2 d-none d-lg-block"><a class="nav-link apptogglefullscreen" id="navbar-fullscreen" href="javascript:;"><i class="ft-maximize font-medium-3 "></i></a></li>
-                    <li class="nav-item nav-search"><a class="nav-link nav-link-search light-gray" href="javascript:"><i class="ft-search font-medium-3"></i></a>
+                    <li class="nav-item mr-2 d-none d-lg-block"><a class="nav-link apptogglefullscreen text-light-gray" id="navbar-fullscreen" href="javascript:;"><i class="ft-maximize font-medium-3 "></i></a></li>
+                    <li class="nav-item nav-search"><a class="nav-link nav-link-search text-light-gray" href="javascript:"><i class="ft-search font-medium-3"></i></a>
                         <div class="search-input">
-                            <div class="search-input-icon"><i class="ft-search font-medium-3 light-gray"></i></div>
-                            <input class="input light-gray" type="text" placeholder="Explore QCCS..." tabindex="0" data-search="template-search">
-                            <div class="search-input-close"><i class="ft-x font-medium-3"></i></div>
+                            <div class="search-input-icon text-light-gray"><i class="ft-search font-medium-3 light-gray"></i></div>
+                            <input class="input text-light-gray" type="text" placeholder="Explore QCCS..." tabindex="0" data-search="template-search">
+                            <div class="search-input-close text-light-gray"><i class="ft-x font-medium-3"></i></div>
                             <ul class="search-list"></ul>
                         </div>
                     </li>
