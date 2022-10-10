92ff2127cb8781ad05af48003b8e5c3994741794
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-10 14:47:01 +0200

    Update navbar.php

--- casius/partials/navbar.php
+++ casius/partials/navbar.php
@@ -15,7 +15,7 @@ include "header.php";
     <nav class="navbar navbar-expand-lg  header-navbar  navbar-static light-gray">
         <div class="container-fluid navbar-wrapper">
             <div class="navbar-header d-flex">
-                <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center " data-toggle="collapse"><i class="ft-menu font-medium-3 "></i></div>
+                <div class="navbar-toggle menu-toggle d-xl-none d-block float-left align-items-center justify-content-center text-light-gray " data-toggle="collapse"><i class="ft-menu font-medium-3 "></i></div>
                 <ul class="navbar-nav">
                     <li class="nav-item mr-2 d-none d-lg-block"><a class="nav-link apptogglefullscreen text-light-gray" id="navbar-fullscreen" href="javascript:;"><i class="ft-maximize font-medium-3 "></i></a></li>
                     <li class="nav-item nav-search"><a class="nav-link nav-link-search text-light-gray" href="javascript:"><i class="ft-search font-medium-3"></i></a>
