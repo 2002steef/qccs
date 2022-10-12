6db711b4349ab4632c96468636463bc4c45d4d82
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-12 10:41:04 +0200

    functions, page account settings

--- casius/backend/functions.php
+++ casius/backend/functions.php
@@ -196,3 +196,49 @@ function klantModal()
         header("Location:overzicht.php");
     }
 }
+
+function UploadPic()
+{
+    if (isset($_POST['submitpic']) && isset($_FILES['my_image'])) {
+        global $mysqli;
+
+        echo "<pre>";
+        print_r($_FILES['my_image']);
+        echo "</pre>";
+
+        $img_name = $_FILES['my_image']['name'];
+        $img_size = $_FILES['my_image']['size'];
+        $tmp_name = $_FILES['my_image']['tmp_name'];
+        $error = $_FILES['my_image']['error'];
+
+        if ($error === 0) {
+            if ($img_size > 1250000) {
+                $em = "Sorry, your file is too large.";
+                header("Location: overzicht.php?error=$em");
+            } else {
+                $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                $img_ex_lc = strtolower($img_ex);
+
+                $allowed_exs = array("jpg", "jpeg", "png");
+
+                if (in_array($img_ex_lc, $allowed_exs)) {
+                    $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+                    $img_upload_path = 'app-assets/img/uploads/' . $new_img_name;
+                    move_uploaded_file($tmp_name, $img_upload_path);
+
+                    // Insert into Database
+                    $sql_pic = "UPDATE `login` SET `image_url` = ? WHERE id = ?";
+                    $stmt = $mysqli->prepare($sql_pic);
+                    $stmt->bind_param(
+                        "si", $new_img_name, $_SESSION['id']
+                    );
+                    $stmt->execute();
+                    header("Location: account-settings.php");
+                } else {
+                    $em = "You can't upload files of this type";
+                    header("Location: overzicht.php?error=$em");
+                }
+            }
+        }
+    }
+}
