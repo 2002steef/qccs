31823a66ba56b4b85a684beccd51812908f7bc5c
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-12 14:34:08 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -156,7 +156,7 @@ include "partials/header.php";
                                                             <textarea rows="6" type="text" id="editMasseuseParagraafje" class=" form-control-plaintext txtarea" readonly><?= $masseuse["paragraafje"]; ?> </textarea>
                                                         </p>
                                                     </div>
-                                                        <a href="" data-target="#paragraaf<?= $masseuse["masseuseID"] ?>" data-toggle="modal" class="btn btn-outline-light-grey float-end"> <span class="ft-edit">Bewerken</span></a>
+                                                        <a href="" data-target="#paragraaf<?= $masseuse["masseuseID"] ?>" data-toggle="modal" class="btn btn-outline-light-grey float-right"> <span class="ft-edit">Bewerken</span></a>
                                                 </div>
                                             </div>
                                         </div>
