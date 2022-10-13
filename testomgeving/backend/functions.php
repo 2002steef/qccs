7a7375a3280c683bc84b3f1ef581413c088f11bb
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-13 11:54:46 +0200

    Update functions.php

--- testomgeving/backend/functions.php
+++ testomgeving/backend/functions.php
@@ -647,7 +647,7 @@ function MasseuseInfoModal()
 
     while ($masseuse = $resultMasseuse->fetch_array()) {
     ?>
-        <div class="modal fade text-left" id="readMore<?= $masseuse["masseuseID"] ?>" aria-labelledby="myModalLabel2" aria-hidden="true">
+        <div class="modal fade text-left" id="info<?= $masseuse["masseuseID"] ?>" aria-labelledby="myModalLabel2" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
