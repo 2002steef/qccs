fbba800a2b8325fa8d41856e9dd6356c83338049
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-13 11:22:43 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -152,7 +152,7 @@ include "partials/header.php";
                                                 <div class="card-content">
                                                     <div class="card-body hidescroll">
                                                         <p class="m-0">
-                                                            <textarea rows="6" type="text" id="editMasseuseParagraafje" class=" form-control-plaintext txtarea" readonly><?= substr($masseuse["paragraafje"],0, 500);  ?> <span data-target="#readMore<?= $masseuse["masseuseID"] ?>" data-toggle="modal">...</span> </textarea>
+                                                            <textarea rows="6" type="text" id="editMasseuseParagraafje" class=" form-control-plaintext txtarea" readonly><?php $ParagraafKort =  substr($masseuse["paragraafje"],0, 500); echo $ParagraafKort;  ?> <a data-target="#readMore<?= $masseuse["masseuseID"] ?>" data-toggle="modal">...</a> </textarea>
                                                         </p>
                                                     </div>
                                                     <a href="" data-target="#paragraaf<?= $masseuse["masseuseID"] ?>" data-toggle="modal" class="btn btn-outline-light-grey float-right cursor-pointer"> <span class="ft-edit">Bewerken</span></a>
