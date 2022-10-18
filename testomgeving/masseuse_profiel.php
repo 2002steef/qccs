b70371526b66c7e24c64b1c94ef270212d70b7fe
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-18 13:53:48 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -98,7 +98,7 @@ include "partials/header.php";
                                                             </li>
                                                         </ul>
                                                     </div>
-                                                    <a href="" data-target="#diensten<?=$masseuse["skills"] ?>" data-toggle="modal">Bewerk diensten</a>
+                                                    <a href="" data-target="#diensten<?=$masseuse["masseuseID"] ?>" data-toggle="modal">Bewerk diensten</a>
                                                 </div>
                                             </div>
                                         </div>
