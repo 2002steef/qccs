a842d52abcdd535d5e58a499d299b020fc7acb69
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-18 15:04:24 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -100,7 +100,7 @@ include "partials/header.php";
                                                             <li class="d-flex align-items-center">
                                                                 <span>nog een massage</span>
                                                             </li>
-                                                             } ?>
+                                                            <?php } ?>
                                                         </ul>
                                                     </div>
                                                     <a href="" class="btn btn-outline-light-grey float-right float-bottom cursor-pointer" data-target="#diensten<?=$masseuse["masseuseID"] ?>" data-toggle="modal">Bewerk diensten</a>
