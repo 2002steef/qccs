62a7373c4599877e3f624d1012ab6bc5ff689b6c
2002steef <steef.vanderpoel@ictmbo.nl>
2022-10-19 15:33:48 +0200

    update

--- josh/popupTest.php
+++ josh/popupTest.php
@@ -127,13 +127,13 @@ include "backend/voucherFunctions.php";
 
             <div id="popupBackground">
                 <div id="popupContent">
-                    <h1>Weet u zeker dat u bij -masseusebedrijf- een afspraak wilt maken?</h1>
-                    <p>uitleg over hoe het werkt en zomeer regel 1<br>
+                    <h1 class="popupText">Weet u zeker dat u bij -masseusebedrijf- een afspraak wilt maken?</h1>
+                    <p class="popupText">uitleg over hoe het werkt en zomeer regel 1<br>
                         uitleg over hoe het werkt en zomeer regel 2<br>
                         uitleg over hoe het werkt en zomeer regel 3<br>
                         uitleg over hoe het werkt en zomeer regel 4<br>
                         uitleg over hoe het werkt en zomeer regel 5</p>
-                    <button id="closePopup">accepteer voorwaarden <b>&times;</b></button>
+                    <button id="closePopup"  class="popupText">accepteer voorwaarden <b>&times;</b></button>
                 </div>
             </div>
 
