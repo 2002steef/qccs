43aa3c66deebf44939876ec3f92acd5da44cd449
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-19 09:22:43 +0200

    Update masseuse_profiel.php

--- testomgeving/masseuse_profiel.php
+++ testomgeving/masseuse_profiel.php
@@ -71,14 +71,10 @@ include "partials/header.php";
                                                 </div>
                                                 <div class="card-content">
                                                     <div class="card-body">
-                                                        <ul class="list-unstyled mb-0">
-                                                            <?php
-                                                            while ($$masseuse["skills"] <= 5) {?>
+                                                        <ul class="list-unstyled mb-0">    
                                                                 <li class="d-flex align-items-center">
                                                                 <span class="ft-tag"><?= $masseuse["skills"] ?></span>
                                                             </li>
-                                                            <?php } ?>
-                                                            
                                                         </ul>
                                                     </div>
                                                     <a href="" class="btn btn-outline-light-grey float-right float-bottom cursor-pointer" data-target="#diensten<?=$masseuse["masseuseID"] ?>" data-toggle="modal">Bewerk diensten</a>
