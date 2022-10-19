c55f5ef64b31e899ba7fe098612f493c4406f45f
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-19 10:20:50 +0200

    Update page-account-settings.php

--- testomgeving/page-account-settings.php
+++ testomgeving/page-account-settings.php
@@ -66,15 +66,15 @@ include "partials/header.php";
                                     </a>
                                 </li>
                                 <?php
-                                            if($_SESSION["status"] == "masseuse"){
-                                            ?>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="voucher-verzileren-tab" data-toggle="tab" href="#voucher-verzileren" role="tab" aria-controls="voucher-verzileren" aria-selected="false">
-                                        <i class="ft-lock mr-1 align-middle"></i>
-                                        <span class="align-middle">Voucher verzilveren</span>
-                                    </a>
-                                </li>
-                                <?php }?>
+                                if ($_SESSION["status"] == "masseuse") {
+                                ?>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="voucher-verzileren-tab" data-toggle="tab" href="#voucher-verzileren" role="tab" aria-controls="voucher-verzileren" aria-selected="false">
+                                            <i class="ft-lock mr-1 align-middle"></i>
+                                            <span class="align-middle">Voucher verzilveren</span>
+                                        </a>
+                                    </li>
+                                <?php } ?>
                             </ul>
                         </div>
                         <div class="col-md-9">
@@ -85,48 +85,36 @@ include "partials/header.php";
                                         <div class="tab-content">
                                             <!-- General Tab -->
                                             <div class="tab-pane active" id="general" role="tabpanel" aria-labelledby="general-tab">
-                                            <?php 
-                                            if($_SESSION["status"]!=="medewerker"){
-                                            ?>  
-                                            <div class="media">
-                                                    <img src="img/uploads/<?php if ($_SESSION["status"] == "bedrijf") {
-                                                                                echo $rowBd["profielFoto"];
-                                                                            } elseif ($_SESSION["status"] == "masseuse") {
-                                                                                echo $rowMs["profielFoto"];
-                                                                            } ?>" alt="profile-img" class="rounded mr-3" height="64" width="64">
-                                                    <div class="media-body">
-                                                        <div class="col-12 d-flex flex-sm-row flex-column justify-content-start px-0 mb-sm-2">
-                                                            <form action="page-account-settings.php" method="post" enctype="multipart/form-data">
-                                                                <input type="file" name="my_image" type="button" class="btn btn-sm btn-primary mb-1 mb-sm-0">
-                                                                <input type="submit" class="btn btn-sm btn-primary mb-1 mb-sm-0" name="submitpic" value="Upload">
-                                                            </form>
+                                                <?php
+                                                if ($_SESSION["status"] !== "medewerker") {
+                                                ?>
+                                                    <div class="media">
+                                                        <img src="img/uploads/<?php if ($_SESSION["status"] == "bedrijf") {
+                                                                                    echo $rowBd["profielFoto"];
+                                                                                } elseif ($_SESSION["status"] == "masseuse") {
+                                                                                    echo $rowMs["profielFoto"];
+                                                                                } ?>" alt="profile-img" class="rounded mr-3" height="64" width="64">
+                                                        <div class="media-body">
+                                                            <div class="col-12 d-flex flex-sm-row flex-column justify-content-start px-0 mb-sm-2">
+                                                                <form action="page-account-settings.php" method="post" enctype="multipart/form-data">
+                                                                    <input type="file" name="my_image" type="button" class="btn btn-sm btn-primary mb-1 mb-sm-0">
+                                                                    <input type="submit" class="btn btn-sm btn-primary mb-1 mb-sm-0" name="submitpic" value="Upload">
+                                                                </form>
+                                                            </div>
+                                                            <p class="text-muted mb-0 mt-1 mt-sm-0">
+                                                                <small>Allowed JPG, GIF or PNG. Max size of 800kB</small>
+                                                                <?php if (isset($err)) {
+                                                                    echo $err;
+                                                                }  ?>
+                                                            </p>
                                                         </div>
-                                                        <p class="text-muted mb-0 mt-1 mt-sm-0">
-                                                            <small>Allowed JPG, GIF or PNG. Max size of 800kB</small>
-                                                            <?php if (isset($err)) {
-                                                                echo $err;
-                                                            }  ?>
-                                                        </p>
                                                     </div>
-                                                </div>
-                                                <?php }?>
+                                                <?php } ?>
                                                 <hr class="mt-1 mt-sm-2">
                                                 <form method="post" action="page-account-settings.php">
                                                     <div class="row">
                                                         <div class="col-12 form-group">
-                                                            <label for="username">Username</label>
-                                                            <div class="controls">
-                                                                <input type="text" id="username" name="username" class="form-control" placeholder="Username" value="<?php if ($_SESSION["status"] == "bedrijf") {
-                                                                                                                                                                        echo $rowBd["userName"];
-                                                                                                                                                                    } elseif($_SESSION["status"]=="masseuse") {
-                                                                                                                                                                        echo $rowMs["userName"];
-                                                                                                                                                                    }elseif($_SESSION["status"]=="medewerker") {
-                                                                                                                                                                        echo $rowMw["userName"];
-                                                                                                                                                                    } ?>" required>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-12 form-group">
-                                                            <label for="name">Name</label>
+                                                            <label for="name">Account Naam</label>
                                                             <div class="controls">
                                                                 <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="<?php if ($_SESSION["status"] == "bedrijf") {
                                                                                                                                                             echo $rowBd["userName"];
@@ -154,15 +142,6 @@ include "partials/header.php";
                                                                                                                                                                 } ?>" required>
                                                             </div>
                                                         </div>
-                                                        <div class="col-12">
-                                                            <div class="alert bg-light-warning alert-dismissible mb-2">
-                                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                                                    <span aria-hidden="true"><i class="ft-x font-medium-2 text-bold-700"></i></span>
-                                                                </button>
-                                                                <p class="mb-0">Your email is not confirmed. Please check your inbox.</p>
-                                                                <a href="javascript:;" class="alert-link">Resend confirmation</a>
-                                                            </div>
-                                                        </div>
                                                         <div style="color: red" class="error-message"><?php
                                                                                                         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                                                                                             if (isset($error)) {
@@ -174,15 +153,15 @@ include "partials/header.php";
                                                         <div class="col-12 d-flex flex-sm-row flex-column justify-content-end">
                                                             <?php
                                                             if ($_SESSION["status"] == "bedrijf") { ?>
-                                                                <button type="submit" name="saveBd" class="btn btn-primary mr-sm-2 mb-1">Save Changes</button>
+                                                                <button type="submit" name="saveBd" class="btn btn-primary mr-sm-2 mb-1">Opslaan</button>
                                                                 <a class="btn btn-outline-light-gray mr-sm-2 mb-1-" href="bma_bedrijfs_klanten_overzicht.php?bedrijfID=<?= $_SESSION["id"] ?>">
-                                                                    Cancel
+                                                                    Annuleren
                                                                 </a>
                                                             <?php
                                                             } elseif ($_SESSION["status"] == "masseuse") { ?>
-                                                                <button type="submit" name="saveMs" class="btn btn-primary mr-sm-2 mb-1">Save Changes</button>
+                                                                <button type="submit" name="saveMs" class="btn btn-primary mr-sm-2 mb-1">Opslaan</button>
                                                                 <a class="btn btn-outline-light-gray mr-sm-2 mb-1-" href="masseuse_profiel.php?masseuseID=<?= $_SESSION["id"] ?>">
-                                                                    Cancel
+                                                                    Annuleren
                                                                 </a>
                                                             <?php
                                                             }
@@ -220,20 +199,20 @@ include "partials/header.php";
                                             </div>
                                             <!-- voucher Tab -->
                                             <?php
-                                            if($_SESSION["status"] == "masseuse"){
+                                            if ($_SESSION["status"] == "masseuse") {
                                             ?>
-                                            <div class="tab-pane" id="voucher-verzileren" role="tabpanel" aria-labelledby="voucher-verzileren-tab">
-                                                <form method="post">
-                                                    <div class="form-group">
-                                                        <label for="voucher-verzilveren">voucher-verzilveren</label>
-                                                        <div class="controls">
-                                                            <input type="text" name="voucherCode" id="voucherCode" class="form-control" placeholder="Vul voucher in" required>
+                                                <div class="tab-pane" id="voucher-verzileren" role="tabpanel" aria-labelledby="voucher-verzileren-tab">
+                                                    <form method="post">
+                                                        <div class="form-group">
+                                                            <label for="voucher-verzilveren">voucher-verzilveren</label>
+                                                            <div class="controls">
+                                                                <input type="text" name="voucherCode" id="voucherCode" class="form-control" placeholder="Vul voucher in" required>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-<?php }
-?>
+                                                    </form>
+                                                </div>
+                                            <?php }
+                                            ?>
                                             <!-- Notifications Tab -->
 
                                         </div>
