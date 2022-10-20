96d6cf3d47c46acd6f64e86339fe7ad8c91d59ce
JoshuaMons <72073739+JoshuaMons@users.noreply.github.com>
2022-10-20 10:16:58 +0200

    login function

--- casius/backend/fnctLogin.php
+++ casius/backend/fnctLogin.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     header("Location:../index.php?login=foutecombi");
                 }
             }
-            $stmtMs->close();
+            $stmt->close();
         }
     }
 }
