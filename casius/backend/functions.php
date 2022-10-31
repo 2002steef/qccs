31c4abb48499df38bd858973bfaf1e0e25cf566e
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-31 12:20:28 +0100

    tab content update

--- casius/backend/functions.php
+++ casius/backend/functions.php
@@ -269,7 +269,7 @@ function klantModal()
                                                                             <div class="form-group row">
                                                                                 <label class="col-md-3 col-form-label" for="horizontal-form-7">Omschrijving</label>
                                                                                 <div class="col-md-9">
-                                                                                    <input type="text" value="<?= $klant["titel"] ?>" class="form-control square" id="horizontal-form-7" name="titel">
+                                                                                    <input type="text" value="<?= $klant["omschrijving"] ?>" class="form-control square" id="horizontal-form-7" name="titel">
                                                                                 </div>
                                                                             </div>
                                                                         </div>
