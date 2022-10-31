720c5b921b3c63e4e949f548d73739ab60de858b
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-10-31 12:03:39 +0100

    tab update content

--- casius/backend/functions.php
+++ casius/backend/functions.php
@@ -246,17 +246,13 @@ function klantModal()
                                                                         <div class="col-12">
                                                                             <div class="form-group row">
                                                                                 <label class="col-md-3 col-form-label" for="horizontal-form-5">Gewenst</label>
-                                                                                <div class="col-md-9">
-                                                                                <fieldset class="form-group">
-                                                                                    <select name="categorie" class="custom-select" id="customSelectMd">
-                                                                                        <option selected hidden><?= $klant["categorie"] ?></option>
-                                                                                    </select>
-                                                                                </fieldset>
+                                                                                <div class="col-md-4">
+                                                                                    <input type="text" value="<?= $klant["categorie"] ?>" class="form-control square" id="horizontal-form-7" name="titel">
                                                                                 </div>
                                                                             </div>
                                                                             <div class="form-group row">
-                                                                                <label class="col-md-3 col-form-label" for="horizontal-form-7">Titel</label>
-                                                                                <div class="col-md-9">
+                                                                                <label class="col-md-6 col-form-label" for="horizontal-form-7">Titel</label>
+                                                                                <div class="col-md-6">
                                                                                     <input type="text" value="<?= $klant["titel"] ?>" class="form-control square" id="horizontal-form-7" name="titel">
                                                                                 </div>
                                                                             </div>
