d9a394f90dfc01775711ee81162f1b5d20dbe3f4
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-11-02 09:13:24 +0100

    overzicht update

--- casius/overzicht.php
+++ casius/overzicht.php
@@ -1,7 +1,7 @@
 <!--Op deze pagina komt een overzicht met alle bedrijven die de applicatie gebruiken-->
 <?php
 include "backend/functions.php";
-
+ToevoegenKlanten();
 klantInfo();
 klantModal();
     include "partials/header.php";
@@ -437,7 +437,7 @@ klantModal();
     </div>
     <?php
     include "partials/footer.php";
-    ToevoegenKlanten();
+    
     //ToevoegenTest();
     ?>
     <script>
