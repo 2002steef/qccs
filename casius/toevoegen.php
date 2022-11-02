b8d735e22d9961243c830bbff34de8fc7ef58589
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-11-02 10:41:21 +0100

    test update

--- casius/toevoegen.php
+++ casius/toevoegen.php
@@ -274,7 +274,7 @@ if (isset($_POST["toevoegenKlant"])) {
         $sql = "INSERT INTO `klanten`(`match_datum`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Email`,
         `Telefoonnummer`, `straat`, `postcode`, `plaats`, `huisnummer`, `huisnummerToevoeging`, `opmerkingen`,
         `categorie`, `sub_categorie`, `titel`, `omschrijving`, `materiaal`, `klant_wensen`, `offertes`, `nagebeld`,
-        `gewenste_aanvang`, `afspraakdatum`, `klant_score`*/)
+        `gewenste_aanvang`, `afspraakdatum`, `klant_score`)
         VALUES
         (?,?,?,?,?,
          ?,?,?,?,?,
@@ -286,7 +286,7 @@ if (isset($_POST["toevoegenKlant"])) {
                 $_POST["match-datum"],$_POST["voornaam"],$_POST["tussenvoegsel"],$_POST["achternaam"],$_POST["email"]
                 ,$_POST["telefoonnummer"] ,$_POST["straat"],$_POST["postcode"],$_POST["plaats"],$_POST["huisnummer"],$_POST["toevoeging"],
                 $_POST["opmerkingen"],$_POST["categorieSelect"],$_POST["sub-categorie"],$_POST["titel"],$_POST["omschrijving"]
-                ,$_POST["materiaal"],$_POST["klant-wensen"],$_POST["offertes"],$_POST["nagebeld"],$_POST["gewenste-aanvang"],$afspraakdatum,($_POST["klant_score"]*/);
+                ,$_POST["materiaal"],$_POST["klant-wensen"],$_POST["offertes"],$_POST["nagebeld"],$_POST["gewenste-aanvang"],$afspraakdatum,($_POST["klant_score"]);
         $stmt->execute();
         $stmt->close();
 }
