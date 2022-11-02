bf45b7743121f69585a077ff6b3e9a86ced033c4
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-11-02 10:29:44 +0100

    test updates

--- casius/toevoegen.php
+++ casius/toevoegen.php
@@ -271,12 +271,12 @@
  include "backend/db.php";
 if (isset($_POST["toevoegenKlant"])) {
     global $mysqli;
-        $sql = "INSERT INTO `klanten`(`match_datum`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Email`/*,
+        $sql = "INSERT INTO `klanten`(`match_datum`, `Voornaam`, `Tussenvoegsel`/*, `Achternaam`, `Email`,
         `Telefoonnummer`, `straat`, `postcode`, `plaats`, `huisnummer`, `huisnummerToevoeging`, `opmerkingen`,
         `categorie`, `sub_categorie`, `titel`, `omschrijving`, `materiaal`, `klant_wensen`, `offertes`, `nagebeld`,
         `gewenste_aanvang`, `afspraakdatum`, `klant_score`*/)
         VALUES
-        (?,?,?,?,?/*,
+        (?,?,?/*,?,?,
          ?,?,?,?,?,
          ?,?,?,?,?,
          ?,?,?,?,?,
@@ -298,8 +298,8 @@ if (isset($_POST["toevoegenKlant"])) {
 		    $opmerkingen = " ";
 	    }
         $stmt->bind_param('sssss',
-                $_POST["match-datum"],$_POST["voornaam"],$tussenvoegsel,$_POST["achternaam"],$_POST["email"]
-                /*,$_POST["telefoonnummer"] ,$_POST["straat"],$_POST["postcode"],$_POST["plaats"],$_POST["huisnummer"],$toevoeging,
+                $_POST["match-datum"],$_POST["voornaam"],$tussenvoegsel/*,$_POST["achternaam"],$_POST["email"]
+                ,$_POST["telefoonnummer"] ,$_POST["straat"],$_POST["postcode"],$_POST["plaats"],$_POST["huisnummer"],$toevoeging,
                 $opmerkingen,$_POST["categorieSelect"],$_POST["sub-categorie"],$_POST["titel"],$_POST["omschrijving"]
                 ,$_POST["materiaal"],$_POST["klant-wensen"],$_POST["offertes"],$_POST["nagebeld"],$_POST["gewenste-aanvang"],$afspraakdatum,$klantScore*/);
         $stmt->execute();
