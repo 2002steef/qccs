0377baf47078a68891528ecc3db870da98a2a878
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-11-02 11:33:00 +0100

    test update

--- casius/toevoegen.php
+++ casius/toevoegen.php
@@ -271,22 +271,22 @@
  include "backend/db.php";
 if (isset($_POST["toevoegenKlant"])) {
     global $mysqli;
-        $sql = "INSERT INTO `klanten`(`match_datum`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Email`,
+        $sql = "INSERT INTO `klanten`(`match_datum`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Email`/*,
         `Telefoonnummer`, `straat`, `postcode`, `plaats`, `huisnummer`, `huisnummerToevoeging`, `opmerkingen`,
         `categorie`, `sub_categorie`, `titel`, `omschrijving`, `materiaal`, `klant_wensen`, `offertes`, `nagebeld`,
-        `gewenste_aanvang`, `afspraakdatum`, `klant_score`)
+        `gewenste_aanvang`, `afspraakdatum`, `klant_score`*/)
         VALUES
-        (?,?,?,?,?,
+        (?,?,?,?,?/*,
          ?,?,?,?,?,
          ?,?,?,?,?,
          ?,?,?,?,?,
-         ?,?,?)";
+         ?,?,?*/)";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('sssssssssssssssssssssss',
-                $_POST["match-datum"],$_POST["voornaam"],$_POST["tussenvoegsel"],$_POST["achternaam"],$_POST["email"]
+        $stmt->bind_param('sssss',
+                $_POST["match-datum"],$_POST["voornaam"],$_POST["tussenvoegsel"],$_POST["achternaam"],$_POST["email"]/*
                 ,$_POST["telefoonnummer"] ,$_POST["straat"],$_POST["postcode"],$_POST["plaats"],$_POST["huisnummer"],$_POST["toevoeging"],
                 $_POST["opmerkingen"],$_POST["categorieSelect"],$_POST["sub-categorie"],$_POST["titel"],$_POST["omschrijving"]
-                ,$_POST["materiaal"],$_POST["klant-wensen"],$_POST["offertes"],$_POST["nagebeld"],$_POST["gewenste-aanvang"],$_POST["afspraakdatum"],$_POST["klant_score"]);
+                ,$_POST["materiaal"],$_POST["klant-wensen"],$_POST["offertes"],$_POST["nagebeld"],$_POST["gewenste-aanvang"],$_POST["afspraakdatum"],$_POST["klant_score"]*/);
         $stmt->execute();
         $stmt->close();
 }
