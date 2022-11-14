e456fef113921c34ef21992d9212e91e21ddf45e
2002steef <steef.vanderpoel@ictmbo.nl>
2022-11-14 11:25:48 +0100

    Update voucherVerzilveren.php

--- testomgeving/voucherVerzilveren.php
+++ testomgeving/voucherVerzilveren.php
@@ -10,7 +10,7 @@ include "partials/header.php";
 ?>
 <!-- BEGIN : Body-->
 
-<body class="vertical-layout vertical-menu 2-columns navbar-static" data-bg-img="bg-glass-1" data-menu="vertical-menu" data-col="2-columns">
+<body class="vertical-layout vertical-menu 2-columns navbar-static" data-bg-img="bg-glass-1" data-menu="vertical-menu">
     <?php
     include "partials/navbar.php";
     ?>
