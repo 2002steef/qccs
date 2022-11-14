14f55aab00259a14ca96987f2b779062cbe877b0
2002steef <steef.vanderpoel@ictmbo.nl>
2022-11-14 11:04:44 +0100

    Update voucherVerzilveren.php

--- testomgeving/voucherVerzilveren.php
+++ testomgeving/voucherVerzilveren.php
@@ -4,46 +4,11 @@ include "backend/voucherFunctions.php";
 
 <!DOCTYPE html>
 <html class="loading" lang="en">
-<!-- BEGIN : Head-->
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="Apex admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Apex admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
-    <title>Voucher verzilveren</title>
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/ico/favicon.ico">
-    <link rel="shortcut icon" type="image/png" href="assets/img/ico/favicon-32.png">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-touch-fullscreen" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900%7CMontserrat:300,400,500,600,700,800,900" rel="stylesheet">
-    <!-- BEGIN VENDOR CSS-->
-    <!-- font icons-->
-    <link rel="stylesheet" type="text/css" href="assets/fonts/simple-line-icons/style.css">
-    <link rel="stylesheet" type="text/css" href="assets/fonts/feather/style.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendors/css/perfect-scrollbar.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendors/css/prism.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendors/css/switchery.min.css">
-    <!-- END VENDOR CSS-->
-    <!-- BEGIN APEX CSS-->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/themes/layout-dark.css">
-    <link rel="stylesheet" href="assets/css/plugins/switchery.css">
-    <!-- END APEX CSS-->
-    <!-- BEGIN Page Level CSS-->
-    <link rel="stylesheet" type="text/css" href="assets/css/core/menu/horizontal-menu.css">
-    <!-- END Page Level CSS-->
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <!-- END: Custom CSS-->
-</head>
+<!-- BEGIN : Head-->
+<?php
+include "partials/header.php";
+?>
 <!-- END : Head-->
 
 <!-- BEGIN : Body-->
@@ -61,11 +26,11 @@ include "backend/voucherFunctions.php";
             <div class="main-content">
                 <div class="content-overlay"></div>
                 <div class="content-wrapper">
-                    <div class="row">
+                    <!-- <div class="row">
                         <div class="col-12">
                             <div class="content-header">Buttons</div>
                         </div>
-                    </div>
+                    </div> -->
                     <!-- Block level buttons start -->
                     <section id="block-level-buttons">
                         <div class="row">
