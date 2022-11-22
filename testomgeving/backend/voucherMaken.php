90f852aed4aa99cc006725f4a2d0ad473756e903
2002steef <steef.vanderpoel@ictmbo.nl>
2022-11-22 13:50:06 +0100

    update phpMAil

--- testomgeving/backend/voucherMaken.php
+++ testomgeving/backend/voucherMaken.php
@@ -1,5 +1,8 @@
 <?php
 // session_start();
+require_once('phpMailer/src/PHPMailer.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use phpMailer\src\Exception;
 
 include("functions.php");
 include "voucherPDF.php";
@@ -47,43 +50,17 @@ function voucherGebruiken()
     $voucher = createRandomVoucher();
     InsertVoucher($voucher);
     $email = getEmail();
-    $to = $email;
-    $from = "Admin@bma.nl";
-    $subject = "Voucher code";
-    $file = "vouchers/user" . $_SESSION['id'] . "Voucher.pdf";
-
-    $htmlContent = 
-    '<h3>PHP Email with Attachment by CodexWorld</h3> 
-    <p>This email is sent from the PHP script with attachment.</p>';
-
-    $headers = "From:".$from;
-    
-    $semi_rand = md5(time());
-    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-        "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
-
-    if (!empty($file) > 0) {
-        if (is_file($file)) {
-            $message .= "--{$mime_boundary}\n";
-            $fp =    @fopen($file, "rb");
-            $data =  @fread($fp, filesize($file));
-
-            @fclose($fp);
-            $data = chunk_split(base64_encode($data));
-            $message .= "Content-Type: application/octet-stream; name=\"" . basename($file) . "\"\n" .
-                "Content-Description: " . basename($file) . "\n" .
-                "Content-Disposition: attachment;\n" . " filename=\"" . basename($file) . "\"; size=" . filesize($file) . ";\n" .
-                "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-        }
-    }
-    $message .= "--{$mime_boundary}--";
-    $returnpath = "-f" . $from;
-
-
-    
-    // echo($to. " " .$subject. " " . $msg. " " . $headers);
-    mail($to, $subject, $message, $headers, $returnpath);
+    $bodytext = "
+    <h1>Dit is header in de mail body.</h1><br>
+    <p>paragraaf</p>
+    ";
+    $mailing = new PHPMailer();
+    $mailing->SetFrom('Admin@bma.nl');
+    $mailing->Subject = "Voucher code";
+    $mailing->Body      = $bodytext;
+    $mailing->AddAddress($email);
+    $mailing->AddAttachment( "../vouchers/user".$_SESSION['id']."Voucher.pdf");
+    $mailing->Send();
     header("location: ../voucherGebruikt.php");
 }
 
