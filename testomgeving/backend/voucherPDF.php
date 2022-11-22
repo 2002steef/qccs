6865e6e7e56835200975a1b62634bcb3111e6214
2002steef <steef.vanderpoel@ictmbo.nl>
2022-11-22 12:57:51 +0100

    Update voucherPDF.php

--- testomgeving/backend/voucherPDF.php
+++ testomgeving/backend/voucherPDF.php
@@ -2,10 +2,11 @@
 include "functions.php";
 require "pdfp/fpdf.php";
 $datum = date("d F Y");
-$fileNaam = "user" . $_SESSION['id'] . "Voucher";
+$fileNaam = "user" . $_SESSION['id'] . "Voucher.pdf";
 
+fopen($fileNaam, 'w+');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 18);
 $pdf->Cell(60, 20, 'je voucher is:');
-$pdf->Output('../vouchers/' . $fileNaam . '.pdf');
+$pdf->Output('../vouchers/' . $fileNaam);
