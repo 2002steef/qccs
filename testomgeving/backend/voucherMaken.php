fcbe3a63e197f9c35940ce7f99f1bc025042b1b5
2002steef <steef.vanderpoel@ictmbo.nl>
2022-11-22 13:24:23 +0100

    update mailPDF

--- testomgeving/backend/voucherMaken.php
+++ testomgeving/backend/voucherMaken.php
@@ -2,7 +2,7 @@
 // session_start();
 
 include("functions.php");
-
+include "voucherPDF.php";
 function createRandomVoucher(
     int $length = 10,
     string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -37,7 +37,7 @@ function getEmail()
     global $mysqli;
     $result = $mysqli->query($sql);
     $rows = $result->fetch_assoc();
-    return($rows['email']);
+    return ($rows['email']);
     // return $result;
 }
 
@@ -49,11 +49,42 @@ function voucherGebruiken()
     $email = getEmail();
     $to = $email;
     $subject = "Voucher code";
-    $msg = "Uw voucher code is . $voucher ";
-    $msg = wordwrap($msg, 70);
-    $headers = "From: Admin@bma.nl";
+    $from = "Admin@bma.nl";
+    // $msg = "Uw voucher code is . $voucher ";
+    // $msg = wordwrap($msg, 70);
+
+    $htmlContent = ' 
+    <h3>PHP Email with Attachment by CodexWorld</h3> 
+    <p>This email is sent from the PHP script with attachment.</p>';
+
+    $headers = "From:".$from;
+    $file = "vouchers/user" . $_SESSION['id'] . "Voucher.pdf";
+    $semi_rand = md5(time());
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+        "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
+
+    if (!empty($file) > 0) {
+        if (is_file($file)) {
+            $message .= "--{$mime_boundary}\n";
+            $fp =    @fopen($file, "rb");
+            $data =  @fread($fp, filesize($file));
+
+            @fclose($fp);
+            $data = chunk_split(base64_encode($data));
+            $message .= "Content-Type: application/octet-stream; name=\"" . basename($file) . "\"\n" .
+                "Content-Description: " . basename($file) . "\n" .
+                "Content-Disposition: attachment;\n" . " filename=\"" . basename($file) . "\"; size=" . filesize($file) . ";\n" .
+                "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+        }
+    }
+    $message .= "--{$mime_boundary}--";
+    $returnpath = "-f" . $from;
+
+
+    
     // echo($to. " " .$subject. " " . $msg. " " . $headers);
-    mail($to, $subject, $msg, $headers);
+    mail($to, $subject, $message, $headers, $returnpath);
     header("location: ../voucherGebruikt.php");
 }
 
--- testomgeving/backend/voucherPDF.php
+++ testomgeving/backend/voucherPDF.php
@@ -1,5 +1,6 @@
 <?php
-include "functions.php";
+
+function voucherPDF($voucher){
 require "pdfp/fpdf.php";
 $datum = date("d F Y");
 $fileNaam = "user" . $_SESSION['id'] . "Voucher.pdf";
@@ -9,5 +10,6 @@ fclose($f);
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 18);
-$pdf->Cell(60, 20, 'je voucher is:');
+$pdf->Cell(60, 20, 'je voucher is: '.$voucher);
 $pdf->Output('F', '../vouchers/' . $fileNaam);
+}
