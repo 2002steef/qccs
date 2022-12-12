d6c3e0e5ab62044833a43ac8aa10507499ee299c
2002steef <steef.van.der.poel@gmail.com>
2022-12-12 14:22:28 +0100

    Update voucherMaken.php

--- testomgeving/backend/voucherMaken.php
+++ testomgeving/backend/voucherMaken.php
@@ -58,9 +58,9 @@ function voucherGebruiken()
     // $mail->send();
     // print_r($mail);
     // header("location: ../voucherGebruikt.php");
-    $testmsg = "bericht";
-    mail("steef.van.der.poel@gmail.com", "test", $testmsg);
 }
 
+    $testmsg = "bericht";
+    mail("steef.van.der.poel@gmail.com", "test", $testmsg);
 
 voucherGebruiken();
