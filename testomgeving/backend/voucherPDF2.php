bfa58d25e2a6c124d22fe726190f90d039db0e92
2002steef <steef.vanderpoel@ictmbo.nl>
2023-01-10 10:34:51 +0100

    Update voucherPDF2.php

--- testomgeving/backend/voucherPDF2.php
+++ testomgeving/backend/voucherPDF2.php
@@ -4,15 +4,19 @@ use Dompdf\Dompdf;
 
 require_once 'dompdf/autoload.inc.php';
 
-function voucherPDF2($voucher){
-$dompdf = new Dompdf;
-$html = '<h1>dit is een header</h1>';
-$html .= '<p>dit is een paragraaf</p>';
-$dompdf->loadHtml($html);
-$dompdf->render();
-// $dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
-$customSize = array(0, 0, 360, 360);
-$dompdf->setPaper($customSize);
-$output = $dompdf->output();
-file_put_contents("../vouchers/voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", $output);
+function voucherPDF2($voucher)
+{
+    $newFile = fopen("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", 'W+');
+    fclose($newFile);
+
+    $dompdf = new Dompdf;
+    $html = '<h1>dit is een header</h1>';
+    $html .= '<p>dit is een paragraaf</p>';
+    $dompdf->loadHtml($html);
+    $dompdf->render();
+    // $dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
+    $customSize = array(0, 0, 360, 360);
+    $dompdf->setPaper($customSize);
+    $output = $dompdf->output();
+    file_put_contents("../vouchers/voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", $output);
 }
