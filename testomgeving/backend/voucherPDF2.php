62e805470e4c2bc0c4361754c0fee2bd9cdcd430
2002steef <steef.vanderpoel@ictmbo.nl>
2023-01-10 10:32:04 +0100

    Update voucherPDF2.php

--- testomgeving/backend/voucherPDF2.php
+++ testomgeving/backend/voucherPDF2.php
@@ -10,7 +10,7 @@ $html = '<h1>dit is een header</h1>';
 $html .= '<p>dit is een paragraaf</p>';
 $dompdf->loadHtml($html);
 $dompdf->render();
-$dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
+// $dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
 $customSize = array(0, 0, 360, 360);
 $dompdf->setPaper($customSize);
 $output = $dompdf->output();
