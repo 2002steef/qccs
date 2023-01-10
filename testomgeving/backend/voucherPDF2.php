974291f77c533efde62e12497e5e931d732c1340
2002steef <steef.vanderpoel@ictmbo.nl>
2023-01-10 13:34:26 +0100

    update voucher

--- testomgeving/backend/voucherPDF2.php
+++ testomgeving/backend/voucherPDF2.php
@@ -11,16 +11,126 @@ function voucherPDF2($voucher)
     // fclose($f);
 
     $dompdf = new Dompdf;
-    $html = '<h1>dit is een header</h1>';
-    $html .= '<p>dit is een paragraaf</p>';
+    $html ='
+        <head>
+            <style>
+                * {
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+
+                main {
+                    background-color: lightgray;
+                    width: 60%;
+                    position: absolute;
+                    top: 25vh;
+                    left: 2vw;
+                }
+
+                div div p {
+                    background-color: white;
+                    color: black;
+                    margin: 4px;
+                    width: 350px;
+                    height: 20px;
+                }
+
+                h2 {
+                    font-size: 35px;
+                    transform: rotate(-90deg);
+                    position: absolute;
+                    right: 24vw;
+                    top: 42vh;
+                }
+
+                h6 {
+                    font-size: 12px;
+                    transform: rotate(-90deg);
+                    position: absolute;
+                    right: 25vw;
+                    top: 43.5vh;
+                }
+
+                #masseuseContactTotal{
+                    display: flex;
+                }
+                #masseuseContact1, #masseuseContact2 {
+                    display: flex;
+                    flex-direction: column;
+                }
+                
+            </style>
+        </head>
+        <body>
+            <main>
+                <h1>Bedrijfs massage abonnement</h1>
+                <p><b>Uitgegeven voor:</b></p>
+                <div id="box">
+                    <div id="companyname" class="form-group">
+                        <p>[bedrijfNaamPlaceHolder]</p>
+                    </div>
+                    <div id="name" class="form-group">
+                        <p for="name">[naamPlaceHolder]</p>
+                    </div>
+                    <div id="email" class="form-group">
+                        <p for="email">[emailPlaceHolder]</p>
+                    </div>
+                    <div id="phone" class="form-group">
+                        <p for="phone">[phonePlaceHolder]</p>
+                    </div>
+                    <p><b>Contactgegevens Massagebedrijfsnaam:</b></p>
+                    <div id="masseuseContactTotal">
+                        <div id="masseuseContact1">
+                            <div id="name" class="form-group">
+                                <p for="name">[masseusePlaceHolder]</p>
+                            </div>
+                            <div>
+                                <div id="name" class="form-group">
+                                    <p for="name">[phonePlaceHolder]</p>
+                                </div>
+                                <div id="name" class="form-group">
+                                    <p for="name">[masseuseEmailPlaceHolder]</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="masseuseContact2">
+                            <div>
+                                <div id="name" class="form-group">
+                                    <p for="name">[postalAndCityPlaceHolder]</p>
+                                </div>
+                            </div>
+                            <div id="name" class="form-group">
+                                <p for="name">[streetAndHouseNumberPlaceHolder]</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p id="text-under">De voucher code is enkel af te nemen bij [masseusePlaceHolder].
+                    <br>
+                    U kunt contact opnemen met uw contactpersoon met de onderstaande gegevens voor het maken van een afspraak.
+                </p>
+            </main>
+            <h6>
+                Hier vind u uw BMA vouchercode
+                <br>
+                Deze kunt u verzilveren bij: [masseusePlaceHolder]
+            </h6>
+            <h2>[voucherPlaceHolder]</h2>
+        </body>
+    ';
+
+
+
     $dompdf->loadHtml($html);
 
-    $customSize = array(0, 0, 1000, 300);
+    $customSize = array(0, 0, 1000, 400);
     $dompdf->setPaper($customSize);
-    
+
     $dompdf->render();
     // $dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
-    
+
     $output = $dompdf->output();
     file_put_contents("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", $output);
 }
