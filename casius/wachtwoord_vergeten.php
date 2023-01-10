9a63084cdc23c8071b4137a6bc2af25cbba24bce
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2023-01-10 11:50:26 +0100

    update wachtwoord vergeten pagina

--- casius/wachtwoord_vergeten.php
+++ casius/wachtwoord_vergeten.php
@@ -13,9 +13,44 @@ include "partials/header.php";
 <body class="horizontal-layout horizontal-menu horizontal-menu-padding navbar-static 1-column auth-page navbar-static" data-bg-img="bg-glass-2" data-open="hover" data-menu="horizontal-menu" data-col="1-column">
 	<!-- ////////////////////////////////////////////////////////////////////////////-->
 	<div class="wrapper">
-		<!-- BEGIN : Main Content-->
+		<div class="main-panel">
+			<!-- BEGIN : Main Content-->
+			<div class="main-content">
+				<div class="content-overlay"></div>
+				<div class="content-wrapper">
+					<!--Forgot Password Starts-->
+					<section id="forgot-password" class="auth-height">
+						<div class="row full-height-vh m-0 d-flex align-items-center justify-content-center">
+							<div class="col-md-7 col-12">
+								<div class="card overflow-hidden">
+									<div class="card-content">
+										<div class="card-body auth-img">
+											<div class="row m-0">
+												<div class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center text-center auth-img-bg py-2">
+													<img src="../../../app-assets/img/gallery/forgot.png" alt="" class="img-fluid" width="260" height="230" />
+												</div>
+												<div class="col-lg-6 col-md-12 px-4 py-3">
+													<h4 class="mb-2 card-title">Recover Password</h4>
+													<p class="card-text mb-3">Please enter your email address and we'll send you instructions on how to reset your password.</p>
+													<input type="email" class="form-control mb-3" placeholder="Email" />
+													<div class="d-flex flex-sm-row flex-column justify-content-between">
+														<a href="auth-login.html" class="btn bg-light-primary mb-2 mb-sm-0">Back To Login</a>
+														<button class="btn btn-primary ml-sm-1">Recover</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<!--Forgot Password Ends-->
 
-		<!-- END : End Main Content-->
+				</div>
+			</div>
+			<!-- END : End Main Content-->
+		</div>
 	</div>
 </body>
 <!-- END : Body-->
