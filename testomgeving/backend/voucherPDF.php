64e4eb3afd5cb91517a4e35264127df1345823b5
2002steef <steef.vanderpoel@ictmbo.nl>
2023-01-10 09:56:37 +0100

    update dompdf

--- testomgeving/backend/voucherPDF.php
+++ testomgeving/backend/voucherPDF.php
@@ -1,15 +1,34 @@
 <?php
 
-function voucherPDF($voucher){
-require "pdfp/fpdf.php";
-$datum = date("d F Y");
-$fileNaam = "voucherpdf/user" . $_SESSION['id'] . "Voucher".$voucher.".pdf";
+use Dompdf\Dompdf;
+use Google\Service\AlertCenter\Attachment;
 
-$f = fopen($fileNaam, 'w+');
-fclose($f);
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 18);
-$pdf->Cell(60, 20, 'je voucher is: '.$voucher);
-$pdf->Output('F', $fileNaam);
+require_once 'dompdf/autoload.inc.php';
+
+function voucherPDF($voucher)
+{
+    require "pdfp/fpdf.php";
+    $datum = date("d F Y");
+    $fileNaam = "voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf";
+
+    $f = fopen($fileNaam, 'w+');
+    fclose($f);
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', 'B', 18);
+    $pdf->Cell(60, 20, 'je voucher is: ' . $voucher);
+    $pdf->Output('F', $fileNaam);
+}
+
+function voucherPDF2($voucher)
+{
+    $dompdf = new Dompdf;
+    $html = '<h1>dit is een header</h1>';
+    $html .= '<p>dit is een paragraaf</p>';
+    $dompdf->loadHtml($html);
+    $dompdf->render();
+    $dompdf->stream("voucherpdf/user" . $_SESSION['id'] . "Voucher" . $voucher . ".pdf", ["Attachment" => 0]);
+    $customSize = array(0,0,360,360);
+    $dompdf->setPaper($customSize);
+
 }
