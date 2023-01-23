a7a0fbd54f84e4b795c259d24daab7225358fb24
JoshuaMons <72073739+JoshuaMons@users.noreply.github.com>
2023-01-23 01:09:45 +0100

    update casius inlogfunctie

--- casius/backend/fnctLogin.php
+++ casius/backend/fnctLogin.php
@@ -19,40 +19,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             if ($stmt->num_rows > 0) {
                 $stmt->bind_result($id, $userName, $password, $email,$rank);
-                //            $stmt->bind_result($id, $password);
                 $stmt->fetch();
-                // Account exists, now we verify the password.
-                // Note: remember to use password_hash in your registration file to store the hashed passwords.
-                //            if (password_verify($_POST['wachtwoord'], $password)) {
                 if ($_POST['wachtwoord'] == $password) {
-                    //                $row = $stmt->get_result();
-                    // Verification success! User has logged-in!
-                    // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
                     session_regenerate_id();
                     $_SESSION['loggedin'] = true;
                     $_SESSION['name'] = $username;
                     $_SESSION['id'] = $id;
                     $_SESSION['rank'] = $rank;
-                    //            echo 'Welcome ' . $_SESSION['name'] . '!';
-                    if (!empty($_POST["remember_me"])) {
-                        setcookie("username", $_POST["email"], time() + 3600);
-                        setcookie("password", $_POST["wachtwoord"], time() + 3600);
-                        //                    echo $_COOKIE['username'];
-                        //                    echo $_COOKIE['password'];
-                        echo "Cookies Set Successfuly";
-                    } else {
-                        setcookie("username", "");
-                        setcookie("password", "");
-                        echo "Cookies Not Set";
-                    }
                     header("Location:../overzicht.php");
-                } 
-            }
-            elseif($stmt->num_rows < 1 ) {
+                }
+                else {
                     // Incorrect password
                     header("Location:../index.php?login=foutecombi");
                 }
-            $stmt->close();
+            }
         }
     }
 }
